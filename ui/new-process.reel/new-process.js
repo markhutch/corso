b68fc918a9746cd98c51eac7bce539fae650afe2
markhutch <noreply>
2014-09-27 06:07:57 +0000

    Add component New Process

---
+++ ui/new-process.reel/new-process.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/new-process.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class NewProcess
+ * @extends Component
+ */
+exports.NewProcess = Component.specialize(/** @lends NewProcess# */ {
+    constructor: {
+        value: function NewProcess() {
+            this.super();
+        }
+    }
+});
