42586e9453cdf9b1f21fcb4afb249b9ba92dd7ef
markhutch <noreply>
2014-10-01 17:13:32 +0000

    Add component tools

---
+++ ui/tools.reel/tools.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tools.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tools
+ * @extends Component
+ */
+exports.Tools = Component.specialize(/** @lends Tools# */ {
+    constructor: {
+        value: function Tools() {
+            this.super();
+        }
+    }
+});
